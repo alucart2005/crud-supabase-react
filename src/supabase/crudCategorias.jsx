20b810a262a72d3bd465ec88e3c6afcbade077ff
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-04-29 17:08:24 -0500

    Feature: 2-3

--- src/supabase/crudCategorias.jsx
+++ src/supabase/crudCategorias.jsx
@@ -22,6 +22,16 @@ export async function InsertarCategorias(p, idauthUserSupabase, file) {
       // rescue URL
       const dataImagen = await subirImagen(idauthUserSupabase, idNuevo, file);
       // edit table image field with URL rescued
+      const parametroStorageEditar = {
+        imagen: dataImagen.publicUrl,
+        id: idNuevo,
+      };
+      await EditarCategorias(
+        parametroStorageEditar,
+        p.idusuario,
+        file,
+        idauthUserSupabase
+      );
     }
   } catch (error) {
     alert(error.error_description || error.message + " insert categories");
@@ -66,5 +76,53 @@ export async function EditarCategorias(p, idusuario, file, idauthUserSupabase) {
     }
   } catch (error) {
     alert(error.error_description || error.message + " edit categories");
+  }
+}
+export async function EliminarCategoria(id, idusuario, idauthUserSupabaseId) {
+  try {
+    const { error } = await supabase
+      .from("categorias")
+      .delete()
+      .eq("idusuario", idusuario)
+      .eq("id", id);
+    if (error) {
+      alert("Delete error ", error);
+    }
+    const ruta = idauthUserSupabaseId + "/categorias" + id;
+    await supabase.storage.from("imagenes").remove([ruta]);
+  } catch (error) {
+    alert(error.error_description || error.message + " delete category");
+  }
+}
+export async function MostrarCategorias(idusuario) {
+  try {
+    const { data, error } = await supabase
+      .from("categorias")
+      .select()
+      .eq("idusuario", idusuario)
+      .order("id", { ascending: false });
+    if (error) {
+      alert("Category error ", error);
+    }
+    return data;
+  } catch (error) {
+    alert(error.error_description || error.message + " show category");
+  }
+}
+export async function BuscarCategorias(buscador, idusuario) {
+  try {
+    const { data, error } = await supabase
+      .from("categorias")
+      .select()
+      .limit(10)
+      .eq("idusuario", idusuario)
+      .ilike("descripcion", "%" + buscador + "%");
+    if (error) {
+      alert("Search category error ", error);
+    }
+    return data;
+  } catch (error) {
+    alert(error.error_description || error.message + " search category");
+
   }
 }
