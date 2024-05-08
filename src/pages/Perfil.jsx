dc72f9d6dcd66a780765d0790b70db5c3d2bcd0e
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-05-08 18:42:18 -0500

    Feature: 7-1

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,12 +1,24 @@
 import styled from "styled-components";
+import { UserAuth } from "../context/AuthContext";
 
 export function Perfil() {
+  const { user } = UserAuth();
   return (
-    <Container>
-      <h1>Perfil</h1>
-    </Container>
+    <SubContainer>
+      <Container>
+        <div className="profile-image">
+          <div className="borde">
+            <img src={user.picture} />
+          </div>
+        </div>
+        <h2 className="profile-username">{user.name}</h2>
+        <small className="profile-useremail">{user.email}</small>
+        <div className="profile-actions">
+          <button className="btnvolver">Back</button>
+        </div>
+      </Container>
+    </SubContainer>
   );
 }
-const Container = styled.div`
-
-`;
+const Container = styled.div``;
+const SubContainer = styled.div``;
