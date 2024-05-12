07e4e079492cf4e9b3ab6bb496ecf864dff90014
Napoleon Anaya R <napoleonanaya@gmail.com>
2024-05-11 19:46:15 -0500

    Feature: 8-2

--- src/context/CrudContext.jsx
+++ src/context/CrudContext.jsx
@@ -1,12 +1,20 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 import { InsertarCategorias } from "../supabase/crudCategorias";
 import { supabase } from "../supabase/supabase.config";
+import { MostrarUsuarioXIdAuthSupabase } from "../supabase/crudUsuarios";
 
-const CrudContext = createContext;
-export function CrudContextProvider() {
+const CrudContext = createContext();
+export function CrudContextProvider({children}) {
+  const { dataUsuarios, setDatausuarios } = useState([]);
   async function insertarCategorias(p, file) {
     const idAuthSupabase = await obtenerAuthSupabaseXuser();
     await InsertarCategorias(p, idAuthSupabase, file);
+  }
+  async function mostrarUsuarios() {
+    const idAuthSupabase = await obtenerAuthSupabaseXuser();
+    const data = await MostrarUsuarioXIdAuthSupabase(idAuthSupabase);
+    setDatausuarios(data[0]);
+    return data[0];
   }
   async function obtenerAuthSupabaseXuser() {
     const {
@@ -18,4 +26,12 @@ export function CrudContextProvider() {
       return idAuthSupabase;
     }
   }
+  return (
+    <CrudContext.Provider value={dataUsuarios}>
+      {children}
+    </CrudContext.Provider>
+  )
+}
+export const CrudSupabaseContext =()=>{
+  return useContext(CrudContext)
 }
